5609ea4257739ec92bca3dbc235af69dd539fe26
cjchirag <chirag@chiragjadhwani.com>
2020-07-19 21:23:22 +0530

    first

---
+++ inc/functions_user.php
@@ -0,0 +1,77 @@
+<?php
+
+function getAllUsers() {
+    global $db;
+
+    try {
+        $query = "SELECT * FROM users";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    } catch (Exception $e) {
+        throw $e;
+    }
+
+}
+
+function getUserById ($id) {
+    global $db;
+
+    try {
+        $statement = $db->prepare('SELECT * FROM users WHERE id=:id');
+        $statement->bindParam('id', $id);
+        $statement->execute();
+        $user = $statement->fetch();
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return $user;
+}
+
+function getUserByName($username) {
+    global $db;
+
+    try {
+        $statement = $db->prepare('SELECT * FROM users WHERE username=:username');
+        $statement = $db->bindParam('username', $username);
+        $statement->execute();
+        $user = $statement->fetch();
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return $user;
+}
+
+function addUser($username, $password) {
+    global $db;
+
+    try {
+        $statement = $db->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
+        $statement->bindParam('username', $username);
+        $statement->bindParam('password', $password);
+        $statement->execute(); 
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return getUserByName($username);
+}
+
+function updateUserPassword($username, $password) {
+
+    global $db;
+
+    try {
+        $statement = $db->prepare('UPDATE users SET password=:password WHERE username=:username');
+        $statement->bindParam('password', $password);
+        $statement->bindParam('username', $username);
+        $statement->execute(); 
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+    return getUserByName($username);
+}
+?>
---
+++ procedures/doRegister.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ .'/../inc/bootstrap.php';
+
+$username = request()->get('username');
+$password = request()->get('password');
+
+$confirmPassword = request()->get('confirm_password');
+
+if ($password != $confirmPassword) {
+  $session->getFlashBag()->add('error', 'Passwords do NOT match');
+  redirect('/register.php');
+}
+
+$user = getUserByName($username);
+if (!empty($user)) {
+  $session->getFlashBag()->add('error', "Could not add $username. It Already Exists");
+  redirect('./register.php');
+}
+
+//$hashed = password_hash($password, PASSWORD_DEFAULT);
+//$user = addUser($username, $hashed);
+//$session->getFlashBag()->add('success', 'User Added');
+//var_dump($user);
+
+// saveUserData($user);
+?> 
