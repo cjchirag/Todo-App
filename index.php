0382258c6e2da9f536fcb4614e9c17d6d512066d
cjchirag <chirag@chiragjadhwani.com>
2020-07-20 23:21:17 +0530

    1.2.2

--- index.php
+++ index.php
@@ -6,10 +6,6 @@ $page = null;
 
 include 'inc/header.php';
 
-// var_dump('The user id is ' . decodeAuthCookie('auth_user_id'));
-
-// var_dump(password_verify('cj', getAuthenticatedUser()['password']));
-
 ?>
     <div class="col-container actions-container">
 
